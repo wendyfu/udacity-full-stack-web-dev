c27e056b447b24882b80aad2b6f91cb797c6ddc5
wendyfu <wendy@gdplabs.id>
2021-07-08 20:47:03 +0700

    Handle filters

---
+++ neighborhood-map/js/app.js
@@ -0,0 +1,44 @@
+const locations = [
+  { name: 'Tokyo Tower', lat: 35.6585848, lng: 139.7432442 },
+  { name: 'Shinjuku Gyoen National Garden', lat: 35.6851806, lng: 139.707863 },
+  { name: 'Tokyo DisneySea', lat: 35.6267151, lng: 139.8828892 },
+  { name: 'Sensō-ji Temple', lat: 35.7147694, lng: 139.7944666 },
+  { name: 'Tokyo Skytree', lat: 35.710067, lng: 139.8085117 },
+];
+
+var Location = function(data) {
+	this.name = ko.observable(data.name);
+	this.lat = ko.observable(data.lat);
+	this.lng = ko.observable(data.lng);
+}
+
+var ViewModel = function() {
+	var self = this;
+
+	this.query = ko.observable("")
+
+	this.query.subscribe(function(_query) {
+		_query = _query.toLowerCase()
+		self.locationList([])
+		self.locationList(
+			locations.filter(item => item.name.toLowerCase().includes(_query))
+				.map(item => new Location(item))
+		)
+		updateMarkers(self.locationList())
+	});
+
+	this.locationList = ko.observableArray([]);
+
+	locations.forEach(function(item) {
+		self.locationList.push(new Location(item));
+	})
+
+	this.clickedLocation = ko.observable();
+
+	this.onLocationClick = function(location) {
+		self.clickedLocation(location);
+		alert(location.name())
+	};
+}
+
+ko.applyBindings(new ViewModel());
--- neighborhood-map/js/maps.js
+++ neighborhood-map/js/maps.js
@@ -1,15 +1,37 @@
+let map;
+let markers = []
+
 // Initialize and add the map
 function initMap() {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.036 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: uluru,
+  map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 35.6928206, lng: 139.8410004 },
+    styles: [{"featureType":"administrative","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"administrative.province","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape","elementType":"all","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","elementType":"all","stylers":[{"saturation":-100},{"lightness":"50"},{"visibility":"simplified"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":"-100"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"all","stylers":[{"lightness":"30"}]},{"featureType":"road.local","elementType":"all","stylers":[{"lightness":"40"}]},{"featureType":"transit","elementType":"all","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]},{"featureType":"water","elementType":"labels","stylers":[{"lightness":-25},{"saturation":-100}]}]
   });
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
+
+  locations.forEach(item => {
+    markers.push(
+      new google.maps.Marker({
+        position: { lat: parseFloat(item.lat), lng: parseFloat(item.lng) },
+        map: map,
+      })
+    );
+  });
+}
+
+// Rerender markers
+function updateMarkers(_locations) {
+  markers.forEach(marker => {
+    marker.setMap(null);
+  })
+
+  markers = []
+  _locations.forEach(item => {
+    markers.push(
+      new google.maps.Marker({
+        position: { lat: parseFloat(item.lat()), lng: parseFloat(item.lng()) },
+        map: map,
+      })
+    );
   });
 }
